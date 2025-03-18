19843fcac1e05c89665dd0c62623978d22055652
interstellar-ferg <aidanjferguson@gmail.com>
2025-03-18 09:23:46 -0600

    saving work here

--- master_scripts/subplot_master.py
+++ master_scripts/subplot_master.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
 
-def subplots(lists):
+def sub_plots(lists):
     length = len(lists)
-    if length >= 1 && length <=3:
-        rows == 1
-        cols == length
+    if 1 <= length <=3:
+        rows = 1
+        cols = length
         position = range(1, length+1)
         
-        fig = plt.figure(figsize = (12,12))
-        
-        for i in range(length):
-            ax = fig.add_subplot(rows, cols, position[i])
-            ax.plot(x[i], y[i], marker = 'o', color = 'r')
-        plt.show()
+        fig = plt.figure(figsize = (9*cols,9))
+        fig.tight_layout(pad = 0)       
 
+    elif length == 4:
+        rows = 2
+        cols = 2
+        position = range(1, length+1)
         
+        fig = plt.figure(figsize = (9*cols,9))
+        fig.tight_layout(pad = 0)
+        
+    else: # length >= 5
+        cols = 3
+        if length%cols == 0:
+            rows = (length//cols)
+        else:
+            rows = (length//cols)+1
+        position = range(1, length+1)
+        
+        fig = plt.figure(figsize = (9*cols,9))
+        fig.tight_layout(pad = 0)
+        
+    return fig, rows, cols, length, position 
